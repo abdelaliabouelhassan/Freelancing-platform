59d598e94498c09cee9b1efdbb0d68de4198057e
abdelali <abdelali54abouelhassan@gmail.com>
2020-04-22 01:44:30 +0000

    first commit le'ts get started (:

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,23 +15,23 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 
-// mix.styles([
-//     'public/css/animate.css',
-//      'public/css/bootstrap.min.css',
-//     'public/css/line-awesome.css',
-//     'public/css/line-awesome-font-awesome.css',
-//     'public/css/font-awesome.min.css',
-//     'public/lib/slick/slick.css',
-//     'public/lib/slick/slick-theme.css',
-//     'public/css/style.css',
-//     'public/css/responsive.css',
-// ], 'public/signin/all.css');
-//
-//
-// mix.scripts([
-//     'public/js/jquery.min.js',
-//     'public/js/popper.js',
-//     'public/js/bootstrap.min.js',
-//     'public/lib/slick/slick.min.js',
-//     'public/js/script.js'
-// ], 'public/signin/all.js');
+mix.styles([
+    'public/css/animate.css',
+     'public/css/bootstrap.min.css',
+    'public/css/line-awesome.css',
+    'public/css/line-awesome-font-awesome.css',
+    'public/css/font-awesome.min.css',
+    'public/lib/slick/slick.css',
+    'public/lib/slick/slick-theme.css',
+    'public/css/style.css',
+    'public/css/responsive.css',
+], 'public/signin/all.css');
+
+
+mix.scripts([
+    'public/js/jquery.min.js',
+    'public/js/popper.js',
+    'public/js/bootstrap.min.js',
+    'public/lib/slick/slick.min.js',
+    'public/js/script.js'
+], 'public/signin/all.js');
