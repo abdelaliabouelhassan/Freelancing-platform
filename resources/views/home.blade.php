5207c9d954cc06258c4a3afdd19aa23cb9d0222c
abdelali <abdelali54abouelhassan@gmail.com>
2020-04-23 20:13:22 +0000

    nothing just testing stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                        Welcome To bridal Platform This Platform Still In Development Mode ! That Is Why You Are Special That You Can See The Progress (:
                 </div>
             </div>
         </div>
