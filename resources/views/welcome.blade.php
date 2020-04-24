ec1338e49784190f508bbaf1bcee27fe575c2dbd
abderrahmane ahmam <abderrahmaneahmam@hotmail.com>
2020-04-24 18:05:16 +0200

    change title name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{config("app.name")." | ".__("Sign up Or Sign in") }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
